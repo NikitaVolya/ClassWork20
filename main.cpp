d59f0b7ba97e0ddeb699c6c110458d83f95c8f84
NikitaVolia <volanskijnikita2@gmail.com>
2024-06-19 19:26:30 +0200

    Add monsters

--- main.cpp
+++ main.cpp
@@ -5,34 +5,73 @@ namespace Bit
 {
 	float transform(float Kbytes) { return Kbytes * 8192; }
 }
-
 namespace Gram
 {
 	float transform(float Kilogram) { return Kilogram * 1000; }
 }
 
+class Monster
+{
+private:
+	float health;
+	float damage;
+	unsigned short level;
+	int xpToLevel;
+protected:
+	Monster(float pHealth, float pDamage, unsigned short pLevel) : health(pHealth), damage(pDamage), level(pLevel), xpToLevel(level* level) {}
+public:
+	Monster() : health(0), damage(0), level(1) {}
+
+	float getHealth() const { return health; }
+	void sendDamage(float pValue) { health = (float) fmax(0, health - pValue); }
+
+	float getDamage() const { return damage + damage * level / 10.f; }
+	void setDamage(float pValue) { damage = pValue; }
+
+	void upLevel() { level += 1; xpToLevel = level * level; }
+
+	int getLevel() const { return (int)level; }
+	void setLevel(int pValue) { if (pValue <= 0) throw std::out_of_range("Level can`t be lower to 0"); level = (unsigned short)pValue; xpToLevel = level * level; }
+	void addXp(float xp) {
+		while (xp > 0) {
+			float tmp = xpToLevel;
+			xpToLevel -= fmin(xpToLevel, xp);
+			xp -= fmin(tmp, xp);
+			if (xpToLevel == 0)
+				upLevel();
+		} 
+	}
+};
+
+namespace Monsters
+{
+	class Ogre : public Monster
+	{
+	public:
+		Ogre() : Monster(10, 15, 1) {};
+	};
+
+	class Goblin : public Monster
+	{
+	public:
+		Goblin() : Monster(5, 3, 1) {};
+	};
+
+	class Skeleton : public Monster
+	{
+	public:
+		Skeleton() : Monster(6, 12, 1) {};
+	};
+}
+
+
 int main()
 {
-	short choice;
-	float number;
-	do {
-		std::cout << "Select type to transform: 1)Bits 2)Grams: ";
-		std::cin >> choice;
-	} while (choice != 1 && choice != 2);
-	std::cout << "Write number of ";
-	switch (choice)
-	{
-	case 1:
-		std::cout << "Kbytes: ";
-		std::cin >> number;
-		std::cout << "Is " << Bit::transform(number) << " bits";
-		break;
-	case 2:
-		std::cout << "Kilograms: ";
-		std::cin >> number;
-		std::cout << "Is " << Gram::transform(number) << " grams";
-		break;
-	}
+	Monsters::Skeleton testSkeleton;
+
+	std::cout << testSkeleton.getLevel() << std::endl;
+	testSkeleton.addXp(658);
+	std::cout << testSkeleton.getLevel();
 
 	return 0;
 }
